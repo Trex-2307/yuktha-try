56eeebf3231100a5c106dd0ea7388d79c75eb298
Trex-2307 <emailmeapoorva@gmail.com>
2021-09-24 14:20:10 +0530

    I made changes with the arry today

--- sketch.js
+++ sketch.js
@@ -1,12 +1,28 @@
-var arr =[1,2,3,4,5,6,7,8,9,10];
+var arr =[1,2,3,4,5];
+// console.log(arr);
 
-function find_num(arr,x)
+function arrSq(a)
 {
-   
+for (var i =0; i<a.length; i++){
+ a[i]=a[i]*a[i];
+
 }
+return a
+}
+
+console.log(arr);
+
+
+
+
 function setup() 
 {
   createCanvas(400, 400);
+
+  var result = arrSq(arr);
+
+  console.log(result);
+
 }
 
 function draw() 
